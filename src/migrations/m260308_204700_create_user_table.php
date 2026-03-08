f916c9e57206f5a1fb967b8e6b0577af92f0ee8a
skulsik <skulsik1@gmail.com>
2026-03-08 21:17:32 +0700

    adding registration + create book

---
+++ src/migrations/m260308_204700_create_user_table.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use yii\db\Migration;
+
+class m260308_204700_create_user_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'password_hash' => $this->string()->notNull(),
+            'auth_key' => $this->string(32),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+        ]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
